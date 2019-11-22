1fbd8b3b6e337c160b621dd88d70caeca17eaa95
kodekite <onesinus231@gmail.com>
2019-11-22 13:11:36 +0700

    Mendapatkan fpb dari 2 bilangan

---
+++ Exercise-2.js
@@ -0,0 +1,20 @@
+function fpb(angka1, angka2) {
+	// you can only write your code here!
+	let angkaTerbesar 	= angka1 > angka2 ? angka1 : angka2;
+	let fpbDuaBilangan 	= 1;
+
+	for(let i = 2; i <= angkaTerbesar; i++){
+		if(angka1 % i === 0 && angka2 % i === 0 && i > fpbDuaBilangan){
+			fpbDuaBilangan = i;			
+		}
+	}
+
+	return fpbDuaBilangan;
+}
+
+// TEST CASES
+console.log(fpb(12, 16)); // 4
+console.log(fpb(50, 40)); // 10
+console.log(fpb(22, 99)); // 11
+console.log(fpb(24, 36)); // 12
+console.log(fpb(17, 23)); // 1 
